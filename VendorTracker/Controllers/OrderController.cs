e1412f7d1deb8a5d1bbbcd70375f143143ff7de1
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-09-25 16:08:31 -0700

    add Views/Order/index.cshtml content

--- VendorTracker/Controllers/OrderController.cs
+++ VendorTracker/Controllers/OrderController.cs
@@ -7,6 +7,29 @@ VendorTracker.Controllers
 {
     public class OrderController : Controller
     {
-      //content
+      [HttpGet("/vendors/{vendorId}/orders/new")]
+      public ActionResult New(int vendorId)
+      {
+        Vendor vendor = Vendor.Find(vendorId);
+        return View(vendor);
+      }
+    
+      [HttpPost("/orders/delete")]
+      public ActionResult DeleteAll()
+      {
+        Vendor.ClearAll();
+        return View();
+      }
+
+      [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+      public ActionResult Show(int vendorId, int orderId)
+      {
+        Order order = Order.Find(orderId);
+        Vendor vendor = Vendor.Find(vendorId);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        model.Add("order", order);
+        model.Add("vendor", vendor);
+        return View(model);
     }
+  }
 }
