bf2cced8dd3c255187a8a3e658561bccc7113307
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-09-25 07:51:20 -0700

    add Vendor constructor to Vendor.cs

--- VendorTracker/Models/Vendor.cs
+++ VendorTracker/Models/Vendor.cs
@@ -4,6 +4,13 @@ namespace VendorTracker.Models
 {
   public class Vendor 
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
+    
+    
     public static void ClearAll()
     {
       _instances.Clear();
