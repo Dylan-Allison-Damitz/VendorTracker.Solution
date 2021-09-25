576690ef5345dfcee3b20ea1448693b0ad4b932a
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-09-25 08:11:04 -0700

    add second passing test for Vendors.cs

--- VendorTracker.Tests/ModelTests/VendorsTests.cs
+++ VendorTracker.Tests/ModelTests/VendorsTests.cs
@@ -21,5 +21,20 @@ namespace VendorTracker.Tests
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
+    [TestMethod]
+        public void GetName_ReturnsName_String()
+        {
+          //Arrange
+          string name = "Test Vendor";
+          string description = "Test Description";
+          Vendor newVendor = new Vendor(name, description);
+
+          //Act
+          string result = newVendor.Name;
+
+          //Assert
+          Assert.AreEqual(name, result);
+        }
+
   }
 }
